7775b0158ddb5f7d5e68464cd5f50d8f146cde6a
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-09-10 21:55:18 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Entidades;
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            Libro libro = new Libro(248, "JAVA en 10 minutos", "Mario Molina", "Manual", "Programación", 10000);
+
+            libro.Mostrar();
+            libro.MostrarPrecio();
+            //libro.MostrarPrecio(10000.43);
+        }
+    }
+}
